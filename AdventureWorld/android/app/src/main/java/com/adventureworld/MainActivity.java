08020401120945abade09f129e5634d5110436d0
Nam-H-Nguyen <nguyenbydoing@gmail.com>
2020-07-31 12:17:09 -0700

    Add RN typescript project

---
+++ AdventureWorld/android/app/src/main/java/com/adventureworld/MainActivity.java
@@ -0,0 +1,15 @@
+package com.adventureworld;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "AdventureWorld";
+  }
+}
